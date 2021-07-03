e4bd1dacb9c59a5e7402b604d62b44f0935246be
IceArrow256 <icearrow256@gmail.com>
2021-07-03 18:57:52 +0300

    no new line in exec (close #1)

--- src/system.cpp
+++ src/system.cpp
@@ -1,6 +1,6 @@
 #include <ia256-utilities/system.hpp>
 
-std::string ia256::exec(std::string const &input, bool is_ret) {
+std::string ia256::exec(std::string const &input, bool no_new_line, bool is_ret) {
   std::array<char, 128> buffer;
   std::string result = "";
   std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(input.c_str(), "r"),
@@ -12,6 +12,9 @@ std::string ia256::exec(std::string const &input, bool is_ret) {
     while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
       result += buffer.data();
     }
+  }
+  if (no_new_line){
+    result.pop_back();
   }
   return result;
 }
